22eb6a21db0fbd933bdf29e3b0f4ba2a959a76d8
stylishjoker <animetplink@gmail.com>
2022-10-03 13:34:56 +0700

    !

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
+import { Fragment } from 'react';
+import { DefaultLayout } from './components/layout';
 function App() {
+    // console.log(DefaultLayout);
     return (
         <Router>
             <div>
                 <Routes>
                     {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        // console.log(Layout);
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
                         const Page = route.component;
-                        return <Route key={index} path={route.path} element={<Page />} />;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
                     })}
                 </Routes>
             </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+import { HeaderOnly } from '~/components/layout';
 
 const publicRoutes = [
     {
@@ -9,6 +13,20 @@ const publicRoutes = [
     {
         path: '/following',
         component: Following,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: Search,
+        layout: null,
     },
 ];
 const privateRoutes = [];
